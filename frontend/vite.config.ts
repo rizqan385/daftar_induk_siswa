eae53fe9885ccb91fe86ee6ff31b7418efe941ee
rizqan385 <fa683822@gmail.com>
2026-05-23 13:20:55 +0700

    add new env

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // listen di 0.0.0.0 supaya bisa diakses dari HP
+    port: 5173,
+  },
 })
+
